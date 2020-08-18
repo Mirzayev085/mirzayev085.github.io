93c108966f3fc9dc7d27f60373add62ecad40bbc
Kamal Mirzayev <mirzayevkamal@gmail.com>
2020-08-18 14:34:42 +0400

    new site

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+    // Configuring Testimonials Slider
+    $('.testimonial-slider').slick();
+    // Configuring Testimonials Slider ends
+
+    // Configuring Owl Carousel in Our Customers section
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                loop: true,
+                autoplay: true
+            },
+            600: {
+                items: 3,
+                loop: true,
+                autoplay: true
+            },
+            1000: {
+                items: 4,
+                loop: false,
+                autoplay: true
+            }
+        }
+    });
+    // Configuring Owl Carousel in Our Customers section ends
+
+});
+
+// Configuring Hamburger menu 
+$(".myhamburger-wrapper").on('click', function() {
+    $(".mymenu-wrapper ul").slideToggle();
+    $(".mymenu-wrapper ul").css("display", "flex")
+});
+// Configuring Hamburger menu ends
+
+
+//Initializind Animate On Scroll
+AOS.init();
+//Initializind Animate On Scroll ends
+
+
+// Starting counter in About Us section
+$('.counter').counterUp();
+//End of counter
+
+
+// Scroll to top configuration starts
+window.onscroll = function isScrolled() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        $(".scroll-to-top").css("display", "grid")
+    } else {
+        $(".scroll-to-top").css("display", "none")
+    }
+};
+
+$(".scroll-to-top").on("click", function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+// Scroll to top configuration ends
